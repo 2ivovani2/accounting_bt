1c004562d7bbdd803f0138c2db09d3507e6eaf7a
Александр <Aleksand.Yaroslavsky@x5.ru>
2022-09-29 17:30:55 +0200

    ïnitial commit to the new branch

--- bbc/bot_constructor/admin.py
+++ bbc/bot_constructor/admin.py
@@ -16,6 +16,7 @@ class CustomUserAdmin(admin.ModelAdmin):
         }),
     )
 
+
 @admin.register(Bot)
 class BotAdmin(admin.ModelAdmin):
     """
@@ -26,7 +27,102 @@ class BotAdmin(admin.ModelAdmin):
 
     fieldsets = (
         ('Bot params', {
-            'fields': ('name', 'token', 'owner', )
+            'fields': ('name', 'token', 'owner', 'functions')
 
         }),
+
+        ('Social media params', {
+            'fields': ('website', 'channel',),
+
+        }),
+    )
+
+
+@admin.register(TGUser)
+class TgUserAdmin(admin.ModelAdmin):
+    """
+        Model describing how to display info about users registered in telegram bots 
+    """
+    list_display = ('telegram_id', 'ton_wallet', 'username',)
+    search_fields = ('id', 'telegram_id', 'ton_wallet', 'username',)
+
+    fieldsets = (
+        ('TgUser\'s main params', {
+            'fields': (
+                ('telegram_id', 'ton_wallet'),
+                ('username', 'is_admin'),
+                'bot'
+            )
+
+       }),
+        ('TgUser\'s additional params', {
+            'fields':(
+                'ref_token',
+                'balance',
+                'experience',
+                'rank',
+                'whitelisted',
+                'messages_amount'
+            )
+        }
+
+        )
+    )
+
+
+@admin.register(NFT)
+class NFTAdmin(admin.ModelAdmin):
+    """
+        Model describing how to display info about NFTS 
+    """
+    list_display = ('id', 'nft_name',)
+    search_fields = ('id', 'nft_name',)
+
+    fieldsets = (
+        ('NFT\'s params', {
+            'fields': (
+                'nft_name',
+                'nft_owner',
+                'nft_creator',
+            )
+
+       }),
+    )
+
+
+@admin.register(ReferralLinks)
+class ReferralLinksAdmin(admin.ModelAdmin):
+    """
+        Model describing how to display info about Reffered links
+    """
+    list_display = ('id',)
+    search_fields = ('id',)
+
+    fieldsets = (
+        ('Refferals\'s params', {
+            'fields': (
+                'referrer',
+                'referred',
+            )
+
+       }),
+    )
+
+@admin.register(Function)
+class FunctionsAdmin(admin.ModelAdmin):
+    """
+        Model describing how to display info about Functions
+    """
+    list_display = ('id', 'function_name')
+    search_fields = ('id', 'function_name')
+
+    fieldsets = (
+        ('Function\'s params', {
+            'fields': (
+                'function_name',
+                'function_desc',
+                'function_sub',
+            )
+
+       }),
     )
--- bbc/bot_constructor/models.py
+++ bbc/bot_constructor/models.py
@@ -1,7 +1,6 @@
+from tabnanny import verbose
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-import random
 
 
 class CustomUser(AbstractUser):
@@ -50,10 +49,51 @@ class CustomUser(AbstractUser):
             verbose_name_plural = 'Web users'
 
 
+class Function(models.Model):
+    """
+        Model describing functions of the bots
+        @SUBSCRIPTION_CHOICES - variants of subscription
+
+    """
+
+    SUBSCRIPTION_CHOICES = (
+        ('default','Default'),
+        ('expanded', 'Expanded'),
+        ('pro','Pro'),
+    )
+
+    function_name = models.CharField(
+        verbose_name='Function name',
+        max_length=50,
+        null=False,
+        default='none',
+    )
+
+    function_desc = models.TextField(
+        verbose_name='Function description',
+        null=False,
+        default='none',
+    )
+
+    function_sub = models.CharField(
+        verbose_name='Type of subscription',
+        max_length=12,
+        null=False,
+        default='default',
+        choices=SUBSCRIPTION_CHOICES, 
+    )
+
+    def __str__(self):
+        return self.function_name
+
+    class Meta:
+            verbose_name = 'Function'
+            verbose_name_plural = 'Functions'
+
+
 class Bot(models.Model):
     """
         Model describing our website user's bots instances
-        # TODO Realise functions and many-to-many
     """
 
     token = models.CharField(
@@ -76,6 +116,23 @@ class Bot(models.Model):
         on_delete=models.CASCADE,
     )
 
+    website = models.CharField(
+        verbose_name='Project website link',
+        max_length=255,
+        null=True,
+    )
+
+    channel = models.CharField(
+        verbose_name='Project channel link',
+        max_length=255,
+        null=True     
+    )
+
+    functions = models.ManyToManyField(
+        Function,
+        verbose_name='Functions included to bot'
+    )
+
     def __str__(self):
         return self.name
 
@@ -87,6 +144,141 @@ class Bot(models.Model):
 class TGUser(models.Model):
     """
         Model describing current user's bot he belongs
-        # TODO Realise later
     """
-    pass
+    
+    telegram_id = models.PositiveBigIntegerField(
+        verbose_name='Telegram user\'s id',
+        null=False,
+        default=0,
+    )
+
+    username = models.CharField(
+        verbose_name='Telegram user\'s username',
+        max_length=50,
+        null=False,
+        default='Ánonymous', 
+    )
+
+    is_admin = models.BooleanField(
+        verbose_name='Bot admin status',
+        null=False,
+        default=False,
+    )
+
+    balance = models.PositiveIntegerField(
+        verbose_name='Telegram user\'s balance',
+        null=False,
+        default=0,
+    )
+
+    experience = models.PositiveIntegerField(
+        verbose_name='Telegram user\'s experience',
+        null=False,
+        default=0,
+    )
+
+    rank = models.CharField(
+        verbose_name='Telegram user\'s rank',
+        max_length=30,
+        null=False,
+        default='Vegetable',
+    )
+
+    ton_wallet = models.CharField(
+        verbose_name='Telegram user\'s TON wallet address',
+        max_length=255,
+        null=False,
+        default=False,
+    )
+
+    whitelisted = models.BooleanField(
+        verbose_name='Telegram user\'s whitelist status',
+        null=False,
+        default=False,
+    )
+
+    messages_amount = models.PositiveIntegerField(
+        verbose_name='Telegram user\'s messages amount',
+        null=False,
+        default=0,
+    )
+
+    ref_token = models.CharField(
+        verbose_name='Telegram user\'s referral token',
+        max_length=255,
+        null=False,
+        default='No token',
+    )
+
+    bot = models.ForeignKey(
+        Bot,
+        verbose_name='Telegram user\'s bot',
+        on_delete=models.CASCADE,
+    )
+    
+    def __str__(self) -> str:
+        return self.username
+
+    class Meta:
+        verbose_name = 'Telegram User'
+        verbose_name_plural = 'Telegram Users'
+
+
+class NFT(models.Model):
+    """
+        Model describing nft
+    """
+
+    nft_name = models.CharField(
+        verbose_name='NFT name',
+        max_length=50,
+        null=False,
+        default='The best NFT in the world',
+    )
+
+    nft_creator = models.ForeignKey(
+        TGUser,
+        verbose_name='NFT creator',
+        on_delete=models.CASCADE,
+    )
+
+    nft_owner = models.ForeignKey(
+        CustomUser,
+        verbose_name='NFT owner',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self) -> str:
+        return self.nft_name
+
+    class Meta:
+        verbose_name = 'NFT'
+        verbose_name_plural = 'NFTS'
+
+
+class ReferralLinks(models.Model):
+    """
+        Model describing refferal links between users
+    """
+
+    referrer = models.ForeignKey(
+        TGUser,
+        verbose_name='Referrer user',
+        related_name='referrer_user',
+        on_delete=models.CASCADE
+    )
+    
+    referred = models.ForeignKey(
+        TGUser,
+        verbose_name='Referred user',
+        related_name='referred_user',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        return self.id
+
+    class Meta:
+        unique_together = (('referrer', 'referred'),)
+        verbose_name = 'Referral link'
+        verbose_name_plural = 'Referral links'
