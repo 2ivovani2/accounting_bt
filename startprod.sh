10a019c03b737848cb2be024e34f20eec151d458
Александр <Aleksand.Yaroslavsky@x5.ru>
2022-10-05 15:06:42 +0200

    докера маму ебал 9

---
+++ startprod.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+docker-compose -f docker-compose.prod.yml exec web python manage.py makemigrations bot_constructor --no-input
+docker-compose -f docker-compose.prod.yml exec web python manage.py migrate bot_constructor --no-input
+docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --no-input
+docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear
