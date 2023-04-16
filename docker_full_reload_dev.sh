cd7f3bf03cfa5e08c284301a56388d8dad7c9266
Alexander <avyaroslavskiy@miem.hse.ru>
2023-04-16 18:41:39 +0300

    add html

--- docker_full_reload_dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker-compose exec web python manage.py makemigrations --no-input
-docker-compose exec web python manage.py migrate --no-input
--- docker_full_reload_prod.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-docker-compose -f docker-compose.prod.yml exec web python manage.py makemigrations bot_constructor --no-input
-docker-compose -f docker-compose.prod.yml exec web python manage.py migrate bot_constructor --no-input
-docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --no-input
