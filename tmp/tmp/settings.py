5004f75b5fa69c0e31736496447a8de58f86727c
Александр <Aleksand.Yaroslavsky@x5.ru>
2023-10-07 01:50:56 +0200

    try to see static gen1

--- tmp/tmp/settings.py
+++ tmp/tmp/settings.py
@@ -131,6 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'main/static',
+]
+
 STATIC_ROOT = BASE_DIR / "staticfiles" 
 
 
